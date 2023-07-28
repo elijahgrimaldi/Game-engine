720e9f9a4274c9cddb433b8fdc0baeac4833e1ba
Elijah <eligrimaldi@berkeley.edu>
2023-07-27 22:08:44 -0700

    added more logging features

--- GameEngine/src/Hazel.h
+++ GameEngine/src/Hazel.h
@@ -2,8 +2,9 @@
 
 // FOR USE IN THE HAZEL APPLICATIONS
 
-#include <stdio.h>
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
+
 
 // ---Entry Point---------------
 #include "Hazel/EntryPoint.h"
